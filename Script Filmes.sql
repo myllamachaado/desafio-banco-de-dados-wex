c5af5069c81e87fa7d5a79f30f0f520813e87859
Mila Machado <myllamachaado@gmail.com>
2025-06-05 14:14:14 +1200

    Update Script Filmes.sql
    Solution.

--- Script Filmes.sql
+++ Script Filmes.sql
@@ -2,6 +2,8 @@ CREATE DATABASE [Filmes]
 GO
 USE [Filmes]
 GO
+-- 1 - Buscar o nome e ano dos Filmes
+SELECT Nome, Ano FROM Filmes;
 
 SET ANSI_NULLS ON
 GO
@@ -18,6 +20,8 @@ CREATE TABLE [dbo].[Atores](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+-- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano FROM Filmes ORDER BY Ano ASC;
 
 SET ANSI_NULLS ON
 GO
@@ -34,6 +38,8 @@ CREATE TABLE [dbo].[ElencoFilme](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+-- 3 - Buscar pelo filme de "De Volta para o Futuro", trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro';
 
 SET ANSI_NULLS ON
 GO
@@ -65,6 +71,8 @@ CREATE TABLE [dbo].[FilmesGenero](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+-- 4 - Buscar os filmes lançados em 1997
+SELECT Nome, Ano FROM Filmes WHERE Ano = 1997;
 
 SET ANSI_NULLS ON
 GO
@@ -325,3 +333,42 @@ REFERENCES [dbo].[Generos] ([Id])
 GO
 ALTER TABLE [dbo].[FilmesGenero] CHECK CONSTRAINT [FK__FilmesGen__IdGen__2E1BDC42]
 GO
+-- 5 - Buscar os filmes lançados APÓS o ano 2000
+SELECT Nome, Ano FROM Filmes WHERE Ano > 2000 ORDER BY Ano ASC;
+
+-- 6 - Buscar os filmes com a duração maior que 100 e menor que 150, ordenando pela duração em ordem crescente
+SELECT Nome, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao ASC;
+
+-- 7 - Buscar a quantidade de filmes lançados por ano, agrupando por ano, ordenando pela duração em ordem decrescente
+SELECT Ano, COUNT(*) AS QuantidadeFilmes 
+FROM Filmes 
+GROUP BY Ano 
+ORDER BY MAX(Duracao) DESC;
+
+-- 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome e UltimoNome
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero = 'M';
+
+-- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT PrimeiroNome, UltimoNome FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome ASC;
+
+-- 10 - Buscar o nome do filme e o gênero
+SELECT Filmes.Nome AS Filme, Generos.Genero AS Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme 
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero;
+
+-- 11 - Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT Filmes.Nome AS Filme, Generos.Genero AS Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme 
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero 
+WHERE Generos.Genero = 'Mistério';
+
+-- 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT Filmes.Nome AS Filme, 
+       Atores.PrimeiroNome AS PrimeiroNome, 
+       Atores.UltimoNome AS UltimoNome, 
+       ElencoFilme.Papel
+FROM Filmes 
+INNER JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+INNER JOIN Atores ON ElencoFilme.IdAtor = Atores.Id;
